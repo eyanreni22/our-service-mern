2ffedfb50faef918f72cf8332cfd59143bea5891
eyanreni22 <devswathi.24@gmail.com>
2025-08-28 15:09:12 +0530

    backend ready for deploy

--- backend/index.js
+++ backend/index.js
@@ -196,6 +196,6 @@ app.use(errorHandler);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
